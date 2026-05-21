a1ee3a6e1f3e707ae65a2ad131fd11bca17f683d
eswarravichandran <eswarr0211@gmail.com>
2026-05-21 23:22:58 +0530

    Removed backend files

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,33 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Login.css'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
-  return (
-    <>
-        <div className='login'>
-            <div className='login-card'>
-                <div>
-                    <h2>Please login to access</h2>
-                    <div className='login-form'>
-                        <div>
-                            <label>Email</label>
-                            <input type='email' placeholder='Enter Email' required/>
-                        </div>
-                        <div>
-                            <label>Password</label>
-                            <input type='password' placeholder='Enter Password' required/>
-                        </div>
-                        <div>
-                            <button type='submit'>Login</button>
-                            <Link to='/'>Forgot password? Click here</Link>
+
+    const [showTab, setShowTab] = useState(false);
+
+    
+    return (
+        <>
+            <div className='login'>
+                <div className='login-card'>
+                    <div>
+                        <h2>Please login to access</h2>
+                        <div className='login-form'>
+                            <div>
+                                <label>Email</label>
+                                <input type='email' placeholder='Enter Email' required/>
+                            </div>
+                            <div>
+                                <label>Password</label>
+                                <input type='password' placeholder='Enter Password' required/>
+                            </div>
+                            <div>
+                                <button type='submit'>Login</button>
+                                <h4 onClick={() => setShowTab(!showTab)}>Forgot password? Click here</h4>
+                            </div>
+                            { showTab && (
+                                <>
+                                <div>
+                                    <label>Please enter valid email</label>
+                                    <input type='email' placeholder='Enter Email' required/>
+                                </div>
+                                <div>
+                                    <button type='submit'>Send Reset Link</button>
+                                </div>
+                                </>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Login
